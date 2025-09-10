4a4562e16eb7c9f7736eda417da2cca7f54f891b
yuvalku <yuvalkutai@gmail.com>
2025-09-10 13:51:55 +0300

    add a clickable link for managing request to the email

--- backend/main.py
+++ backend/main.py
@@ -176,6 +176,7 @@ def create_request(
             f"Dates: {row.start_date} → {row.end_date}\n"
             f"Notes: {row.notes or '-'}\n"
             f"Status: {row.status}\n"
+            f"For managing requests: https://booking-app-com.onrender.com/admin\n"
         )
         background_tasks.add_task(send_email, NOTIFY_EMAIL, subject, body)
 
